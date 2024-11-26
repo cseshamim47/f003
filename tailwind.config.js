0478ee19083541612cb827287809134b0c8d38e7
cseshamim47 <cseshamim47@gmail.com>
2024-11-26 19:16:07 +0600

    make the pricing section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./assets/js/**/*.js", "./components/**/*.html"],
+  content: ["./index.html", "./test.html", "./assets/js/**/*.js", "./components/**/*.html"],
   theme: {
     extend: {
       fontFamily: {
