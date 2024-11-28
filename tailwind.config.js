cb691c574fa03e7424d37c14c931da6c54a586a7
cseshamim47 <cseshamim47@gmail.com>
2024-11-28 17:42:52 +0600

    website done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./test.html", "./assets/js/**/*.js", "./components/**/*.html"],
+  content: [
+    "./index.html",
+    "./test.html",
+    "./assets/js/**/*.js",
+    "./components/**/*.html",
+  ],
   theme: {
     extend: {
       fontFamily: {
         jakarta: ["Plus Jakarta Sans", "sans-serif"],
         poppins: ["Poppins", "sans-serif"],
-        manrope: ["Manrope", "sans-serif"]
+        manrope: ["Manrope", "sans-serif"],
       },
       colors: {
-        customGreen: "#0E6F00"
-      }
+        customGreen: "#0E6F00",
+      },
     },
   },
   plugins: [],
