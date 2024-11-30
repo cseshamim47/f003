482a9b9b262c681264556a532de577c2b717b7ed
cseshamim47 <cseshamim47@gmail.com>
2024-11-30 15:39:57 +0600

    ..

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,37 @@ menuBtn.addEventListener('click', () => {
 closeMenu.addEventListener('click', () => {
   mobileMenu.classList.add('translate-x-full');
 });
+
+
+
+  // Select elements
+  const toggle = document.querySelector('input[type="checkbox"]'); // Toggle input
+  const essentialPrice = document.getElementById('essential-price');
+  const comprehensivePrice = document.getElementById('comprehensive-price');
+
+  // Define pricing options
+  const pricing = {
+    monthly: {
+      essential: '৳ 2,500',
+      comprehensive: '৳ 6,000',
+    },
+    yearly: {
+      essential: '৳ 25,000',
+      comprehensive: '৳ 60,000',
+    },
+  };
+
+  // Update prices based on toggle
+  toggle.addEventListener('change', () => {
+    const isYearly = toggle.checked; // Check if yearly pricing is selected
+    if (isYearly) {
+      // Update to yearly prices
+      essentialPrice.innerHTML = `${pricing.yearly.essential}<sup class="text-[14px] text-[#4B5768] font-normal">/year</sup>`;
+      comprehensivePrice.innerHTML = `${pricing.yearly.comprehensive}<sup class="text-[14px] text-neutral-50 font-normal">/year</sup>`;
+    } else {
+      // Update to monthly prices
+      essentialPrice.innerHTML = `${pricing.monthly.essential}<sup class="text-[14px] text-[#4B5768] font-normal">/month</sup>`;
+      comprehensivePrice.innerHTML = `${pricing.monthly.comprehensive}<sup class="text-[14px] text-neutral-50 font-normal">/month</sup>`;
+    }
+  });
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,15 @@ module.exports = {
     "./components/**/*.html",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
     extend: {
+      screens: {
+        xl: "1280px", // Make 1280px the maximum breakpoint
+        "2xl": "1280px", // Ensure larger breakpoints also stay capped at 1280px
+      },
       fontFamily: {
         jakarta: ["Plus Jakarta Sans", "sans-serif"],
         poppins: ["Poppins", "sans-serif"],
@@ -15,6 +23,10 @@ module.exports = {
       },
       colors: {
         customGreen: "#0E6F00",
+        primary: "#0E6F00",
+        secondary: "#063000",
+        bodyText: "#18181B",
+        star: "#FDB241",
       },
     },
   },
